2e91f5443e03cf4a82674ab1fc7ec0102ac373da
Chynarich-test <chynarich228@gmail.com>
2025-10-04 13:05:05 +0300

    Убрать неиспользующиеся исключения

--- stats-service/stats-server/src/main/java/ru/yandex/practicum/exception/ExceptionHandler.java
+++ stats-service/stats-server/src/main/java/ru/yandex/practicum/exception/ExceptionHandler.java
@@ -7,24 +7,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionHandler {
     @org.springframework.web.bind.annotation.ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ExceptionResponse handleNotFound(final NotFoundException e) {
-        return new ExceptionResponse(
-                e.getMessage()
-        );
-    }
-
-    @org.springframework.web.bind.annotation.ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionResponse handleDuplicatedData(final ValidationException e) {
-        return new ExceptionResponse(
-                e.getMessage()
-        );
-    }
-
-    @org.springframework.web.bind.annotation.ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ExceptionResponse handleExistData(final ExistException e) {
         return new ExceptionResponse(
                 e.getMessage()
         );
